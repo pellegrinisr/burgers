8476f15b5dd9d5e472be666771f77f9b93871bb6
Stephen Pellegrini <spellegrini156@gmail.com>
2018-08-28 20:04:24 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var expressHandlebars = require('express-handlebars');
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, function(error) {
+    if (error) throw error;
+    console.log('connected at port', PORT);
+});
